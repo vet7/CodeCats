033338b93fe462ac3c1545ba06d45a7f45c547a0
Arian Najafi <ariannajafi@Arians-iMac.modem>
2021-04-01 17:40:39 +1100

    Added PersonalProfile.js to show team member profile on click

---
+++ PersonalProfile.js
@@ -0,0 +1,47 @@
+function showProfile1(imgs) {
+  var profileImg = document.getElementById("profileimg");
+  var imgText = document.getElementById("imgtext");
+  profileImg.src = "assets/Personal-Profile-scaled.jpeg";
+  imgText.innerHTML = imgs.alt;
+  profileimg.parentElement.style.display = "block";
+}
+
+function showProfile2(imgs) {
+  var profileImg = document.getElementById("profileimg");
+  var imgText = document.getElementById("imgtext");
+  profileImg.src = "assets/Personal-Profile-scaled.jpeg";
+  imgText.innerHTML = imgs.alt;
+  profileimg.parentElement.style.display = "block";
+}
+
+function showProfile3(imgs) {
+  var profileImg = document.getElementById("profileimg");
+  var imgText = document.getElementById("imgtext");
+  profileImg.src = "assets/Personal-Profile-scaled.jpeg";
+  imgText.innerHTML = imgs.alt;
+  profileimg.parentElement.style.display = "block";
+}
+
+function showProfile4(imgs) {
+  var profileImg = document.getElementById("profileimg");
+  var imgText = document.getElementById("imgtext");
+  profileImg.src = "assets/Personal-Profile-scaled.jpeg";
+  imgText.innerHTML = imgs.alt;
+  profileimg.parentElement.style.display = "block";
+}
+
+function showProfile5(imgs) {
+  var profileImg = document.getElementById("profileimg");
+  var imgText = document.getElementById("imgtext");
+  profileImg.src = "assets/Personal-Profile-scaled.jpeg";
+  imgText.innerHTML = imgs.alt;
+  profileimg.parentElement.style.display = "block";
+}
+
+function showProfile6(imgs) {
+  var profileImg = document.getElementById("profileimg");
+  var imgText = document.getElementById("imgtext");
+  profileImg.src = "assets/Personal-Profile-scaled.jpeg";
+  imgText.innerHTML = imgs.alt;
+  profileimg.parentElement.style.display = "block";
+}
