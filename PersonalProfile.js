32089a7576643a553ce7b319b5a7bad2720940b1
Arian Najafi Yamchelo <45844226+aryna93@users.noreply.github.com>
2021-04-05 12:02:31 +1000

    Added a paragraph to PersonalProfile.js for the overview of the profile

--- PersonalProfile.js
+++ PersonalProfile.js
@@ -1,47 +1,47 @@
 function showProfile1(imgs) {
   var profileImg = document.getElementById("profileimg");
-  var imgText = document.getElementById("imgtext");
   profileImg.src = "assets/Personal-Profile-scaled.jpeg";
-  imgText.innerHTML = imgs.alt;
   profileimg.parentElement.style.display = "block";
+  document.getElementById('name').innerHTML = "name1";
+  document.getElementById("overview").innerHTML = "name 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo ut elit dignissim suscipit. Vivamus a erat nec elit ullamcorper facilisis. Nam vulputate eu sem in maximus. Nulla tempus mattis purus vitae blandit. Fusce neque mauris, imperdiet at massa ut, porta cursus ligula. Integer sit amet sagittis lectus, consequat suscipit mi. Phasellus dignissim ex elit, vitae vulputate metus bibendum non. Phasellus pulvinar libero turpis, fringilla iaculis dui tempus vel. Fusce varius pulvinar libero."
 }
 
 function showProfile2(imgs) {
   var profileImg = document.getElementById("profileimg");
-  var imgText = document.getElementById("imgtext");
   profileImg.src = "assets/Personal-Profile-scaled.jpeg";
-  imgText.innerHTML = imgs.alt;
   profileimg.parentElement.style.display = "block";
+  document.getElementById('name').innerHTML = "name2";
+  document.getElementById("overview").innerHTML = "name 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo ut elit dignissim suscipit. Vivamus a erat nec elit ullamcorper facilisis. Nam vulputate eu sem in maximus. Nulla tempus mattis purus vitae blandit. Fusce neque mauris, imperdiet at massa ut, porta cursus ligula. Integer sit amet sagittis lectus, consequat suscipit mi. Phasellus dignissim ex elit, vitae vulputate metus bibendum non. Phasellus pulvinar libero turpis, fringilla iaculis dui tempus vel. Fusce varius pulvinar libero."
 }
 
 function showProfile3(imgs) {
   var profileImg = document.getElementById("profileimg");
-  var imgText = document.getElementById("imgtext");
   profileImg.src = "assets/Personal-Profile-scaled.jpeg";
-  imgText.innerHTML = imgs.alt;
   profileimg.parentElement.style.display = "block";
+  document.getElementById('name').innerHTML = "name3";
+  document.getElementById("overview").innerHTML = "name 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo ut elit dignissim suscipit. Vivamus a erat nec elit ullamcorper facilisis. Nam vulputate eu sem in maximus. Nulla tempus mattis purus vitae blandit. Fusce neque mauris, imperdiet at massa ut, porta cursus ligula. Integer sit amet sagittis lectus, consequat suscipit mi. Phasellus dignissim ex elit, vitae vulputate metus bibendum non. Phasellus pulvinar libero turpis, fringilla iaculis dui tempus vel. Fusce varius pulvinar libero."
 }
 
 function showProfile4(imgs) {
   var profileImg = document.getElementById("profileimg");
-  var imgText = document.getElementById("imgtext");
   profileImg.src = "assets/Personal-Profile-scaled.jpeg";
-  imgText.innerHTML = imgs.alt;
   profileimg.parentElement.style.display = "block";
+  document.getElementById('name').innerHTML = "name4";
+  document.getElementById("overview").innerHTML = "name 4 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo ut elit dignissim suscipit. Vivamus a erat nec elit ullamcorper facilisis. Nam vulputate eu sem in maximus. Nulla tempus mattis purus vitae blandit. Fusce neque mauris, imperdiet at massa ut, porta cursus ligula. Integer sit amet sagittis lectus, consequat suscipit mi. Phasellus dignissim ex elit, vitae vulputate metus bibendum non. Phasellus pulvinar libero turpis, fringilla iaculis dui tempus vel. Fusce varius pulvinar libero."
 }
 
 function showProfile5(imgs) {
   var profileImg = document.getElementById("profileimg");
-  var imgText = document.getElementById("imgtext");
   profileImg.src = "assets/Personal-Profile-scaled.jpeg";
-  imgText.innerHTML = imgs.alt;
   profileimg.parentElement.style.display = "block";
+  document.getElementById('name').innerHTML = "name5";
+  document.getElementById("overview").innerHTML = "name 5 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo ut elit dignissim suscipit. Vivamus a erat nec elit ullamcorper facilisis. Nam vulputate eu sem in maximus. Nulla tempus mattis purus vitae blandit. Fusce neque mauris, imperdiet at massa ut, porta cursus ligula. Integer sit amet sagittis lectus, consequat suscipit mi. Phasellus dignissim ex elit, vitae vulputate metus bibendum non. Phasellus pulvinar libero turpis, fringilla iaculis dui tempus vel. Fusce varius pulvinar libero."
 }
 
 function showProfile6(imgs) {
   var profileImg = document.getElementById("profileimg");
-  var imgText = document.getElementById("imgtext");
   profileImg.src = "assets/Personal-Profile-scaled.jpeg";
-  imgText.innerHTML = imgs.alt;
   profileimg.parentElement.style.display = "block";
+  document.getElementById('name').innerHTML = "name6";
+  document.getElementById("overview").innerHTML = "name 6 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo ut elit dignissim suscipit. Vivamus a erat nec elit ullamcorper facilisis. Nam vulputate eu sem in maximus. Nulla tempus mattis purus vitae blandit. Fusce neque mauris, imperdiet at massa ut, porta cursus ligula. Integer sit amet sagittis lectus, consequat suscipit mi. Phasellus dignissim ex elit, vitae vulputate metus bibendum non. Phasellus pulvinar libero turpis, fringilla iaculis dui tempus vel. Fusce varius pulvinar libero."
 }
