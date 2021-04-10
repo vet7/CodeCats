edd019a4b5d0dd2d4332fd7a0d1af458af79f558
Jake Kent <jake.m.kent@gmail.com>
2021-04-10 15:43:06 +1000

    Organized file heirarchy, renamed unnamed files

--- scripts/codec4ts.js
+++ scripts/codec4ts.js
@@ -1,6 +1,6 @@
 function showProfile1(imgs) {
   var profileImg = document.getElementById("profileimg");
-  profileImg.src = "assets/Personal-Profile-scaled.jpeg";
+  profileImg.src = "../assets/Personal-Profile-scaled.jpeg";
   profileimg.parentElement.style.display = "block";
   document.getElementById('name').innerHTML = "name1";
   document.getElementById("overview").innerHTML = "name 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo ut elit dignissim suscipit. Vivamus a erat nec elit ullamcorper facilisis. Nam vulputate eu sem in maximus. Nulla tempus mattis purus vitae blandit. Fusce neque mauris, imperdiet at massa ut, porta cursus ligula. Integer sit amet sagittis lectus, consequat suscipit mi. Phasellus dignissim ex elit, vitae vulputate metus bibendum non. Phasellus pulvinar libero turpis, fringilla iaculis dui tempus vel. Fusce varius pulvinar libero."
@@ -8,7 +8,7 @@ function showProfile1(imgs) {
 
 function showProfile2(imgs) {
   var profileImg = document.getElementById("profileimg");
-  profileImg.src = "assets/Personal-Profile-scaled.jpeg";
+  profileImg.src = "../assets/Personal-Profile-scaled.jpeg";
   profileimg.parentElement.style.display = "block";
   document.getElementById('name').innerHTML = "name2";
   document.getElementById("overview").innerHTML = "name 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo ut elit dignissim suscipit. Vivamus a erat nec elit ullamcorper facilisis. Nam vulputate eu sem in maximus. Nulla tempus mattis purus vitae blandit. Fusce neque mauris, imperdiet at massa ut, porta cursus ligula. Integer sit amet sagittis lectus, consequat suscipit mi. Phasellus dignissim ex elit, vitae vulputate metus bibendum non. Phasellus pulvinar libero turpis, fringilla iaculis dui tempus vel. Fusce varius pulvinar libero."
@@ -16,7 +16,7 @@ function showProfile2(imgs) {
 
 function showProfile3(imgs) {
   var profileImg = document.getElementById("profileimg");
-  profileImg.src = "assets/Personal-Profile-scaled.jpeg";
+  profileImg.src = "../assets/Personal-Profile-scaled.jpeg";
   profileimg.parentElement.style.display = "block";
   document.getElementById('name').innerHTML = "name3";
   document.getElementById("overview").innerHTML = "name 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo ut elit dignissim suscipit. Vivamus a erat nec elit ullamcorper facilisis. Nam vulputate eu sem in maximus. Nulla tempus mattis purus vitae blandit. Fusce neque mauris, imperdiet at massa ut, porta cursus ligula. Integer sit amet sagittis lectus, consequat suscipit mi. Phasellus dignissim ex elit, vitae vulputate metus bibendum non. Phasellus pulvinar libero turpis, fringilla iaculis dui tempus vel. Fusce varius pulvinar libero."
@@ -24,7 +24,7 @@ function showProfile3(imgs) {
 
 function showProfile4(imgs) {
   var profileImg = document.getElementById("profileimg");
-  profileImg.src = "assets/Personal-Profile-scaled.jpeg";
+  profileImg.src = "../assets/Personal-Profile-scaled.jpeg";
   profileimg.parentElement.style.display = "block";
   document.getElementById('name').innerHTML = "name4";
   document.getElementById("overview").innerHTML = "name 4 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo ut elit dignissim suscipit. Vivamus a erat nec elit ullamcorper facilisis. Nam vulputate eu sem in maximus. Nulla tempus mattis purus vitae blandit. Fusce neque mauris, imperdiet at massa ut, porta cursus ligula. Integer sit amet sagittis lectus, consequat suscipit mi. Phasellus dignissim ex elit, vitae vulputate metus bibendum non. Phasellus pulvinar libero turpis, fringilla iaculis dui tempus vel. Fusce varius pulvinar libero."
@@ -32,7 +32,7 @@ function showProfile4(imgs) {
 
 function showProfile5(imgs) {
   var profileImg = document.getElementById("profileimg");
-  profileImg.src = "assets/Personal-Profile-scaled.jpeg";
+  profileImg.src = "../assets/Personal-Profile-scaled.jpeg";
   profileimg.parentElement.style.display = "block";
   document.getElementById('name').innerHTML = "name5";
   document.getElementById("overview").innerHTML = "name 5 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo ut elit dignissim suscipit. Vivamus a erat nec elit ullamcorper facilisis. Nam vulputate eu sem in maximus. Nulla tempus mattis purus vitae blandit. Fusce neque mauris, imperdiet at massa ut, porta cursus ligula. Integer sit amet sagittis lectus, consequat suscipit mi. Phasellus dignissim ex elit, vitae vulputate metus bibendum non. Phasellus pulvinar libero turpis, fringilla iaculis dui tempus vel. Fusce varius pulvinar libero."
@@ -40,7 +40,7 @@ function showProfile5(imgs) {
 
 function showProfile6(imgs) {
   var profileImg = document.getElementById("profileimg");
-  profileImg.src = "assets/Personal-Profile-scaled.jpeg";
+  profileImg.src = "../assets/Personal-Profile-scaled.jpeg";
   profileimg.parentElement.style.display = "block";
   document.getElementById('name').innerHTML = "name6";
   document.getElementById("overview").innerHTML = "name 6 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo ut elit dignissim suscipit. Vivamus a erat nec elit ullamcorper facilisis. Nam vulputate eu sem in maximus. Nulla tempus mattis purus vitae blandit. Fusce neque mauris, imperdiet at massa ut, porta cursus ligula. Integer sit amet sagittis lectus, consequat suscipit mi. Phasellus dignissim ex elit, vitae vulputate metus bibendum non. Phasellus pulvinar libero turpis, fringilla iaculis dui tempus vel. Fusce varius pulvinar libero."
